c748534a791ef4e558256ab4853f6c5f99395b93
Jean Carlos <jeancotillo@proton.me>
2026-05-04 22:22:01 -0300

    feat(course-5): add audio toggle exercise from module 3

---
+++ course-5-react-basics/modulo3/exercicio-selecao-musica/src/App.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+function App() {
+  const bird1 = new Audio("/bird1.mp3");
+
+  // Step 2: Un-comment the bird2 variable below
+  const bird2 = new Audio("/bird2.mp3");
+
+  function toggle1() {
+    if (bird1.paused) {
+      bird1.play();
+    } else {
+      bird1.pause();
+    }
+  }
+
+  // Step 3: Define the toggle2 function here
+  // It should work exactly like toggle1 but use bird2 variable
+  function toggle2() {
+    if (bird2.paused) {
+      bird2.play();
+    } else {
+      bird2.pause();
+    }
+  }
+
+  return (
+    <div>
+      <button onClick={toggle1}>Caspian Tern 1</button>
+      {/* Step 1: Add toggle2 function to the onClick attribute below */}
+      <button onClick={toggle2}>Caspian Tern 2</button>
+    </div>
+  );
+}
+
+export default App;
